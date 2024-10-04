0a5c6bc1d546ba6800e156b4804ce518dc53768a
Benjamin Hanson <blhanson@ucsd.edu>
2024-10-04 16:21:42 -0700

    Updated CR3BP example

--- examples/CR3BP/CR3BP.c
+++ examples/CR3BP/CR3BP.c
@@ -1,11 +1,11 @@
 // CR3BP.c, https://github.com/bhanson10/gbees/tree/main/examples/CR3BP
 // Copyright 2024 by Benjamin Hanson, published under BSD 3-Clause License.
 
-#include "../../gbees.h" 
+#include "../../gbees-hash.h" 
 #include "CR3BP.h"
 
 // This function defines the dynamics model - required
-void CR3BP(double* f, double* x, double* dx, double* coef){
+void CR3BP(double* f, double* x, double t, double* dx, double* coef){
     double r1 = pow(pow(x[0]+coef[0],2) + pow(x[1],2) + pow(x[2],2), 1.5);
     double r2 = pow(pow(x[0]-1+coef[0],2) + pow(x[1],2) + pow(x[2],2), 1.5);
     f[0] = x[3];
@@ -50,14 +50,16 @@ int main(void){
     int NUM_MEAS = 1;                               // Number of measurements
     int DEL_STEP = 20;                              // Number of steps per deletion procedure
     int OUTPUT_FREQ = 20;                           // Number of steps per output to terminal
-    bool OUTPUT = true;                            // Write info to terminal after certain amount of steps
+    int CAPACITY = (int)pow(2,18);                  // Size of hash table (power of 2 for optimal hashing)
+    bool OUTPUT = true;                             // Write info to terminal after certain amount of steps
     bool RECORD = true;                             // Write PDFs to .txt file
     bool MEASURE = false;                           // Take discrete measurement updates
     bool BOUNDS = true;                             // Add inadmissible regions to grid
+    bool COLLISIONS = false;                        // Track collisions
     //==========================================================================================================//
 
     //================================================= GBEES ==================================================//
-    run_gbees(CR3BP, NULL, CR3BP_J, G, M, T, P_DIR, M_DIR, NUM_DIST, NUM_MEAS, DEL_STEP, OUTPUT_FREQ, DIM_h, OUTPUT, RECORD, MEASURE, BOUNDS);
+    run_gbees(CR3BP, NULL, CR3BP_J, G, M, T, P_DIR, M_DIR, NUM_DIST, NUM_MEAS, DEL_STEP, OUTPUT_FREQ, CAPACITY, DIM_h, OUTPUT, RECORD, MEASURE, BOUNDS, COLLISIONS);
 
     return 0;
 }
--- examples/CR3BP/CR3BP.py
+++ examples/CR3BP/CR3BP.py
@@ -1,15 +1,15 @@
-# CR3BP.py, https://github.com/bhanson10/gbees/tree/main/examples/CR3BP
+# CR3BP.py, https://github.com/bhanson10/gbees-hash/tree/main/examples/CR3BP
 # Copyright 2024 by Benjamin Hanson, published under BSD 3-Clause License.
 
 import sys
 sys.path.append('../../')
-import gbeespy as gbees  # type: ignore
+import gbeespyhash as gbees  # type: ignore
 
 DIM_f = 6 # State dimension
 DIM_h = 6 # Measurement dimension
 
 # This function defines the dynamics model - required
-def CR3BP(x, dx, coef):
+def CR3BP(x, t, dx, coef):
     r1 = ((x[0] + coef[0])**2 + (x[1])**2 + (x[2])**2)**1.5
     r2 = ((x[0] - 1 + coef[0])**2 + (x[1])**2 + (x[2])**2)**1.5
     f1 = x[3]
@@ -31,8 +31,8 @@ def CR3BP_J(x, coef):
 print("Reading in initial discrete measurement...\n")
 
 P_DIR = "./results/python"      # Saved PDFs path
-M_DIR = "./measurements"     # Measurement path
-M_FILE = "measurement0.txt"; # Measurement file
+M_DIR = "./measurements"        # Measurement path
+M_FILE = "measurement0.txt"     # Measurement file
 M = gbees.Meas_create(DIM_f, M_DIR, M_FILE)
 #============================================================================================================#
 
@@ -42,20 +42,22 @@ print("Reading in user inputs...\n")
 dx = [None] * DIM_f                             # Grid width, default is half of the std. dev. from the initial measurement 
 for i in range(DIM_f):
     dx[i] = (M.cov[i][i]**(0.5))/2
-G = gbees.Grid_create(DIM_f, 1E-7, M.mean, dx); # Inputs: (dimension, probability threshold, center, grid width)    
+G = gbees.Grid_create(DIM_f, 1E-7, M.mean, dx)  # Inputs: (dimension, probability threshold, center, grid width)    
  
 coef = [2.528017528540000E-5]                   # PCR3BP trajectory attributes (mu)
-T = gbees.Traj_create(len(coef), coef);         # Inputs: (# of coefficients, coefficients)
+T = gbees.Traj_create(len(coef), coef)          # Inputs: (# of coefficients, coefficients)
 
-NUM_DIST = 8;                                   # Number of distributions recorded per measurement
-NUM_MEAS = 4;                                   # Number of measurements
-DEL_STEP = 20;                                  # Number of steps per deletion procedure
-OUTPUT_FREQ = 20;                               # Number of steps per output to terminal
-OUTPUT = True;                                 # Write info to terminal
-RECORD = True;                                  # Write PDFs to .txt file
-MEASURE = False;                                # Take discrete measurement updates
-BOUNDS = True;                                  # Add inadmissible regions to grid
+NUM_DIST = 8                                    # Number of distributions recorded per measurement
+NUM_MEAS = 4                                    # Number of measurements
+DEL_STEP = 20                                   # Number of steps per deletion procedure
+OUTPUT_FREQ = 20                                # Number of steps per output to terminal
+CAPACITY = int(2**18)                           # Size of hash table (power of 2 for optimal hashing)
+OUTPUT = True                                   # Write info to terminal
+RECORD = True                                   # Write PDFs to .txt file
+MEASURE = False                                 # Take discrete measurement updates
+BOUNDS = True                                   # Add inadmissible regions to grid
+COLLISIONS = True                               # Track collisions
 #============================================================================================================#
 
 #================================================== GBEES ===================================================#
-gbees.run_gbees(CR3BP, None, CR3BP_J, G, M, T, P_DIR, M_DIR, NUM_DIST, NUM_MEAS, DEL_STEP, OUTPUT_FREQ, DIM_h, OUTPUT, RECORD, MEASURE, BOUNDS)
+gbees.run_gbees(CR3BP, None, CR3BP_J, G, M, T, P_DIR, M_DIR, NUM_DIST, NUM_MEAS, DEL_STEP, OUTPUT_FREQ, CAPACITY, DIM_h, OUTPUT, RECORD, MEASURE, BOUNDS, COLLISIONS)
