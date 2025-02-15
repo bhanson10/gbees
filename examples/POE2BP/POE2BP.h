688fd49d340c24ed9b32bb1d1c64ebcc1f59c32f
Benjamin Hanson <blhanson@ucsd.edu>
2025-02-15 10:40:37 -0800

    Added POE2BP Example

---
+++ examples/POE2BP/POE2BP.h
@@ -0,0 +1,13 @@
+// POE2BP.h, https://github.com/bhanson10/gbees/tree/main/examples/POE2BP
+// Copyright 2024 by Benjamin Hanson, published under BSD 3-Clause License.
+
+#ifndef POE2BP_H
+#define POE2BP_H
+
+#define DIM_f 2 // State dimension
+
+// This function defines the dynamics model - required
+void POE2BP(double* f, double* x, double t, double* dx, double* coef);
+
+
+#endif
